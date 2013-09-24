a48479e2637aaa2c38381318af75ef2e2ae7b55a
Alan Gorton <dev.code1up@googlemail.com>
2013-09-24 13:37:47 +0100

    wip

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,7 +4,7 @@ public class QueryProcessor {
 
     public String process(String query) {
         if (query.contains("programming")) {
-            return "Computer programming is the blah blah yada yada yada something about computing.";
+            return "Computer computing.";
         }
         return "";
     }
