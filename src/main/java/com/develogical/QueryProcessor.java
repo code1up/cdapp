ddc706932e887e4c6059407ab9f31f974a52c5bc
Alan Gorton <dev.code1up@googlemail.com>
2013-09-24 12:02:25 +0100

    Changed QueryProcessor text

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,8 +4,7 @@ public class QueryProcessor {
 
     public String process(String query) {
         if (query.contains("programming")) {
-            return "Computer programming is the comprehensive process that leads from an original " 
-                   + "formulation of a computing problem to executable programs.";
+            return "Computer programming is the blah blah yada yada yada.";
         }
         return "";
     }
