5a2010c6ce83887a7a8ba2d4f5afd96f9139b806
Alan Gorton <dev.code1up@googlemail.com>
2013-09-24 14:29:19 +0100

    wip

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,8 +1,27 @@
 package com.develogical;
 
+// which%20of%20the%20following%20numbers%20is%20the%20largest:%20875,%2093,%2027,%20223
+
 public class QueryProcessor {
 
     public String process(String query) {
+
+    	if (query.contains("which of the following numbers")) {
+	    	Pattern p = Pattern.compile("-?\\d+");
+
+	    	Matcher m = p.matcher(query);
+	    	int largest = -39743;
+
+	    	while (m.find()) {
+	    		int n = Integer.parseInt(m.group());
+
+	    		if (n > largest) {
+	    			largest = n;
+	    		}
+	    	}
+
+    	}
+
         if (query.contains("programming")) {
             return "Yay, computing.";
         }
